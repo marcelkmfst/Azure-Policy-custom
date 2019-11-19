a9a9a5bc07d4ad178cbdb738656bd23d537009b4
Marcel Keller <marcel.keller.ext@siemens.com>
2019-11-19 16:39:51 +0100

    Added Azure FW Runbooks

---
+++ Runbooks/Runbook-Start-AzureFW.ps1
@@ -0,0 +1,62 @@
+﻿  ########### Runbook by MK to start Azure Firewall instance (change from deallocate) 
+Param
+(
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewall,
+
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallResourceGroup,
+
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallPIP,
+
+
+  
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallVNETName,
+
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallVNETResourceGroup,
+
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallPIPResourceGroup
+  )
+
+import-module Az.Network
+Import-Module Az.Accounts
+Import-Module Az.Automation
+Import-Module Az.Compute
+
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+
+# Start a firewall
+
+$azfw = Get-AzFirewall -Name $AzureFirewall -ResourceGroupName $AzureFirewallResourceGroup
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $AzureFirewallVNETResourceGroup -Name $AzureFirewallVNETName
+$publicip = Get-AzPublicIpAddress -Name $AzureFirewallPIP -ResourceGroupName $AzureFirewallPIPResourceGroup
+$azfw.Allocate($vnet,$publicip)
+Set-AzFirewall -AzureFirewall $azfw
---
+++ Runbooks/Runbook-deallocate-AzureFW.ps1
@@ -0,0 +1,32 @@
+﻿####### Runbook by MK to set AzureFW to deallocated
+Param(
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewall,
+
+  [Parameter (Mandatory= $true)]
+  [String] $AzureFirewallResourceGroup
+)
+
+import-module Az.Network
+Import-Module Az.Accounts
+Import-Module Az.Automation
+Import-Module Az.Compute
+
+$connectionName = "AzureRunAsConnection"
+
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+
+
+# Stop an existing firewall
+
+$azfw = Get-AzFirewall -Name $AzureFirewall -ResourceGroupName $AzureFirewallResourceGroup
+$azfw.Deallocate()
+Set-AzFirewall -AzureFirewall $azfw
