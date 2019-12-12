f4897b0cf083092bf0ec297f09988d6b050d957d
Marcel Keller <marcel.keller.ext@siemens.com>
2019-12-12 18:23:19 +0100

    new files

---
+++ Runbooks/Azure P2S-Create-Self-Signed-Root-and-Client-Cert.ps1
@@ -0,0 +1,16 @@
+﻿### for use with Azure P2S VPN
+### Create a Root Certificate
+
+$cert = New-SelfSignedCertificate -Type Custom -KeySpec Signature `
+-Subject "CN=MK101P2SRootCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" -KeyUsageProperty Sign -KeyUsage CertSign
+
+### Create a Client certificate
+
+New-SelfSignedCertificate -Type Custom -DnsName P2SChildCert -KeySpec Signature `
+-Subject "CN=MK201P2SChildCert" -KeyExportPolicy Exportable `
+-HashAlgorithm sha256 -KeyLength 2048 `
+-CertStoreLocation "Cert:\CurrentUser\My" `
+-Signer $cert -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.2")
+
---
+++ Runbooks/configure-Analysisservices-firewall.ps1
@@ -0,0 +1,12 @@
+﻿### script to dynamically configure Analysis services firewall based on client public IP
+
+$mypublicip = (Invoke-WebRequest -uri "http://ifconfig.me/ip").Content
+$analysisservicesserver = Read-Host "please specify the Puma Analysis Services Server you want to access"
+Add-AzAnalysisServicesAccount -RolloutEnvironment $analysisservicesenvironment
+$firewallrule = New-AzAnalysisServicesFirewallRule -FirewallRuleName "client$mypublic" -RangeStart $mypublicip -RangeEnd $mypublicip
+$firewallruleconfig = New-AzAnalysisServicesFirewallConfig -FirewallRule $firewallrule
+Set-AzAnalysisServicesServer -Name $analysisservicesserver -FirewallConfig $firewallruleconfig
+
+
+
+
